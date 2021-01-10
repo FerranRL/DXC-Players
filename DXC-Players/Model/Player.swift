c4b1d7ee9d0422f3c9671ac399bff4a9ed7d977e
ferranrosales <ferran.ext@soundsmarket.com>
2021-01-10 18:08:11 +0100

    Search bar and request implemented.

--- DXC-Players/Model/Player.swift
+++ DXC-Players/Model/Player.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Player: Decodable {
+struct Player: Codable {
     var player_id: Int?
     var team_id: Int?
     var country_id: Int?
@@ -24,8 +24,28 @@ struct Player: Decodable {
     var height: String?
     var weight: String?
     var image_path: String?
+    
+    public init(player_id: Int?,team_id: Int?, country_id: Int?, position_id: Int?, common_name: String?, display_name: String?, fullname: String?, firstname: String?, lastname: String?, nationality: String?, birthdate: String?, birthcountry: String?, birthplace: String?, height: String?, weight: String?, image_path: String?) {
+        
+        self.player_id = player_id
+        self.team_id = team_id
+        self.country_id = country_id
+        self.position_id = position_id
+        self.common_name = common_name
+        self.display_name = display_name
+        self.fullname = fullname
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nationality = nationality
+        self.birthdate = birthdate
+        self.birthcountry = birthcountry
+        self.birthplace = birthplace
+        self.height = height
+        self.weight = weight
+        self.image_path = image_path
+    }
 }
 
-struct Data: Decodable {
+struct Data: Codable {
     var data:[Player]?
 }
--- DXC-Players/ViewController.swift
+++ DXC-Players/ViewController.swift
@@ -9,29 +9,106 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    var players: [Player]! = []
+    let searchBar = UISearchBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let request = RequestPlayerData()
+        
+        
+
+        configureUI()
         
         
         
-        request.getPlayersSearchData(search: "Fisher") { (result) in
-            try? result.get().data?.forEach {
-                guard let common = $0.common_name else {return}
-                guard let display = $0.display_name else {return}
-                print(common)
-                print(display)
-            }
-        }
+        
         
         
     }
     
 
+    
+    
+    func configureUI() {
+        
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        
+        let app = UINavigationBarAppearance()
+        app.backgroundColor = .systemGreen
+        app.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        self.navigationController?.navigationBar.scrollEdgeAppearance = app
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .white
+        
+        navigationItem.title = "Búsqueda"
+        
+        navigationController?.navigationBar.barStyle = .black
+        
+        showSearchBarButton(shouldShow: true)
+        
+        
+        
+
+        
+    }
+    
+    @objc func handleShowSearchBar() {
+        
+        search(shouldShow: true)
+        searchBar.searchTextField.backgroundColor = .white
+        searchBar.searchTextField.tintColor = .gray
+        searchBar.placeholder = "Nombre del jugador"
+        
+        searchBar.becomeFirstResponder()
+        
+    }
+    
+    func showSearchBarButton(shouldShow: Bool) {
+        if shouldShow {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(handleShowSearchBar))
+        } else {
+            navigationItem.rightBarButtonItem = nil
+        }
+    }
+    
+    func search(shouldShow: Bool) {
+        showSearchBarButton(shouldShow: !shouldShow)
+        searchBar.showsCancelButton = shouldShow
+        navigationItem.titleView = shouldShow ? searchBar : nil
+    }
 
 
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        search(shouldShow: false)
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        self.players = []
+        let request = RequestPlayerData()
+        let playerToSearch = searchBar.searchTextField.text!
+
+        request.getPlayersSearchData(search: playerToSearch) { (result) in
+            try? result.get().data?.forEach {
+//                guard let common = $0.common_name else {return}
+//                guard let display = $0.display_name else {return}
+//                print(common)
+//                print(display)
+                self.players.append(Player(player_id: $0.player_id, team_id: $0.team_id, country_id: $0.country_id, position_id: $0.position_id, common_name: $0.common_name, display_name: $0.display_name, fullname: $0.fullname, firstname: $0.firstname, lastname: $0.lastname, nationality: $0.nationality, birthdate: $0.birthdate, birthcountry: $0.birthcountry, birthplace: $0.birthplace, height: $0.height, weight: $0.weight, image_path: $0.image_path))
+                
+            }
+            print(self.players!)
+            searchBar.searchTextField.text = ""
+        }
+    }
+    
 }
 
