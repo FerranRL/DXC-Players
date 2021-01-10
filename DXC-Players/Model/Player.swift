2738dc61e89641c87c0775d80a5e7357ede880d2
ferranrosales <ferran.ext@soundsmarket.com>
2021-01-10 09:49:41 +0100

    Initial Commit

---
+++ DXC-Players/Model/Player.swift
@@ -0,0 +1,27 @@
+//
+//  Player.swift
+//  DXC-Players
+//
+//  Created by Ferran Rosales on 10/1/21.
+//
+
+import Foundation
+
+struct Player: Codable {
+    var player_id: Int!
+    var team_id: Int!
+    var country_id: Int!
+    var position_id: Int!
+    var common_name: String!
+    var display_name: String!
+    var fullname: String!
+    var firstname: String!
+    var lastname: String!
+    var nationality: String!
+    var birthdate: String!
+    var birthcountry: String!
+    var birthplace: String?
+    var height: String?
+    var weight: String?
+    var image_path: String!
+}
